edc9f133e22498fce7727d913bd89fb68ba0a7dd
Aaqil Shihab <aaqilsh@yahoo.com>
2025-07-08 21:53:13 +0100

    Add DevelopmentConfig class for development environment settings

--- services/config.py
+++ services/config.py
@@ -4,3 +4,6 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "ecoquizsecret")
     QUESTIONS_FILE = os.getenv("QUESTIONS_FILE", "questions.json")
     DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
