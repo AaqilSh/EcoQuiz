b3a218748edb0e31235ba1e8d1db4cf1d08f2934
Aaqil Shihab <aaqilsh@yahoo.com>
2025-07-08 21:51:31 +0100

    Implement admin credentials verification in AuthService

--- services/auth_service.py
+++ services/auth_service.py
@@ -7,4 +7,8 @@ class AuthService:
 		self.admin_password_hash = os.getenv('ADMIN_PASSWORD_HASH', 
                                            generate_password_hash('admin'))
 
+	def verify_admin_credentials(self, username, password):
+		if username == self.admin_username:
+			return check_password_hash(self.admin_password_hash, password)
+		return False
 	
